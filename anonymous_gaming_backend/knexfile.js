98589cbb995e06b68b01c1a1045df03c7da4ba12
Jarae-MacyMan <sjaraebr@gmail.com>
2022-11-04 18:42:00 -0400

    backend set up

---
+++ anonymous_gaming_backend/knexfile.js
@@ -0,0 +1,35 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'anonymous_gaming_DB',
+      user:     'postgres',
+      password: ''
+    },
+  },
+
+
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
