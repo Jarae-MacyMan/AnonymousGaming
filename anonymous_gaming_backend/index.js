474ef86b7dd0b071cfb8fe269c2ac279dd09f224
Jarae-MacyMan <sjaraebr@gmail.com>
2022-11-09 18:03:08 -0500

    FE started

--- anonymous_gaming_backend/index.js
+++ anonymous_gaming_backend/index.js
@@ -10,7 +10,7 @@ const postsRouter  = require('./routes/postsRouter')
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/', (req, res) => {
     res.send("")
---
+++ anonymous_gaming_frontend/src/contex/ContextProvider.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Context from "./context";
+
+const ContextProvider = ({ children }) => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+    email: "",
+    title: ""
+  });
+
+  const [userInfo, setUserInfo] = useState({});
+
+  // const [userQuestions, setUserQuestions] = useState([]);
+
+  // const [allQuestions, setAllQuestions] = useState([]);
+
+  // const [answers, setAnswers] = useState([]);
+
+  // const [question, setQuestion] = useState("");
+
+  // const [newQuestion, setNewQuestion] = useState("")
+
+  // const [answer, setAnswer] = useState("");
+
+  // const [isEditing, setIsEditing] = useState(false)
+
+  const state = {
+    inputs,
+    setInputs,
+    userInfo,
+    setUserInfo,
+    // userQuestions,
+    // setUserQuestions,
+    // question,
+    // setQuestion,
+    // newQuestion,
+    // setNewQuestion,
+    // allQuestions, 
+    // setAllQuestions,
+    // isEditing,
+    // setIsEditing,
+    // answers,
+    // setAnswers,
+    // answer,
+    // setAnswer
+  };
+
+  return <Context.Provider value={state}>{children}</Context.Provider>;
+};
+
+export default ContextProvider;
